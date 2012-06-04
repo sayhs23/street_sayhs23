914003429892cd9f36561299c2e966cca743b231
sayhs23 <sayhs23@nate.com>
2012-06-05 00:07:34 +0900

    a

--- rooms.js
+++ rooms.js
@@ -4,6 +4,7 @@ var http = require("http");
 var sys = require("util");
 var tojson = require('./xml2json');
 var sjson = require("xml2json");
+var Game = require("./Game");
 var fs = require("fs");
 
 var mysql = require('mysql')

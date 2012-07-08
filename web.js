dc0fd2614b4c69d3efaf5f08137e4e549d771233
sayhs23 <sayhs23@nate.com>
2012-07-08 23:22:37 +0900

    test6

--- web.js
+++ web.js
@@ -22,9 +22,9 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+//app.configure('development', function(){
+ // app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+//});
 
 app.configure('production', function(){
   app.use(express.errorHandler()); 
